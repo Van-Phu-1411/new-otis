1c08c48fdca3e56ad81bf992154be08283e02eb1
Van-Phu-1411 <tranvanphu14112001@gmail.com>
2023-03-24 23:15:21 +0700

    11h15pm

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ module.exports = {
   content: ["./**.{html,js}", "./index.html"],
   theme: {
     extend: {
+      borderWidth: {
+        fontSize: {
+          0.6: "0.6rem",
+        },
+        DEFAULT: "1px",
+        0: "0",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        6: "6px",
+        8: "8px",
+        9: "9px",
+        10: "10px",
+        11: "11px",
+        12: "12px",
+        13: "13px",
+        14: "14px",
+        18: "18px",
+      },
+      colors: {
+        rgba: "transparent rgba(251, 146, 60,.9)",
+      },
       padding: {
         full: "100%",
       },
