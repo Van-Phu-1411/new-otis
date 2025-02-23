f96723837f23bdd539cf7a88680a446a754b9f99
Văn Phú <124586272+Van-Phu-1411@users.noreply.github.com>
2025-02-23 12:42:04 +0700

    Update Locket_hvb_fix.js

--- coderhvb-main/Locket_hvb_fix.js
+++ coderhvb-main/Locket_hvb_fix.js
@@ -25,7 +25,7 @@ if (!obj.subscriber.entitlements) obj.subscriber.entitlements = {};
 if (!obj.subscriber.subscriptions) obj.subscriber.subscriptions = {};
 
 // ========= Tạo thông tin gói Locket Gold ========= //
-var hoangvanbao = {
+var vuongducha = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
@@ -41,7 +41,7 @@ var hoangvanbao = {
 var hvb_entitlement = {
   grace_period_expires_date: null,
   purchase_date: specificDate, // Ngày tham gia
-  product_identifier: "com.hoangvanbao.premium.yearly",
+  product_identifier: "com.vuongducha.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z" // Ngày hết hạn lâu dài
 };
 
@@ -50,13 +50,13 @@ const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let entitlementKey = mapping[match][0] || "Locket";
-  let subscriptionKey = mapping[match][1] || "com.hoangvanbao.premium.yearly";
+  let subscriptionKey = mapping[match][1] || "com.vuonducha.premium.yearly";
 
-  obj.subscriber.subscriptions[subscriptionKey] = hoangvanbao;
+  obj.subscriber.subscriptions[subscriptionKey] = vuongducha;
   obj.subscriber.entitlements[entitlementKey] = hvb_entitlement;
 } else {
   // Gán mặc định nếu không có khớp
-  obj.subscriber.subscriptions["com.hoangvanbao.premium.yearly"] = hoangvanbao;
+  obj.subscriber.subscriptions["com.vuongducha.premium.yearly"] = vuongducha;
   obj.subscriber.entitlements["Locket"] = hvb_entitlement;
 }
 
